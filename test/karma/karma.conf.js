643fe881130c0c62fa2a34a68ecd2f16d9147076
André Augusto Tissot <andrehtissot@gmail.com>
2018-01-03 22:27:52 +0100

    Karma config simplified

--- test/karma/karma.conf.js
+++ test/karma/karma.conf.js
@@ -1,16 +1,35 @@
-const {karmaCommon, getKarmaConfig, defaultBrowserMatrix} = require('./karma.common');
-
 module.exports = function (config) {
-  const cfg = getKarmaConfig (defaultBrowserMatrix, {
-    // Base path should point at the root
-    basePath: '../..',
-    // Files to include
-    files: karmaCommon.files.concat([
-      'dist/dexieORMWrapper.js',
-      'test/tmp/tests.js',
-      { watched: true, included: false, served: true, pattern: 'test/worker.js' },
-    ])
-  });
-
-  config.set(cfg);
+    const testWithChrome = true
+    config.set({
+        frameworks: [
+            'qunit'
+        ],
+        reporters: [
+            'mocha'
+        ],
+        client: {
+            captureConsole: false
+        },
+        port: 19144,
+        colors: true,
+        browserNoActivityTimeout: 2 * 60 * 1000,
+        plugins: [
+            'karma-qunit',
+            'karma-mocha-reporter',
+            testWithChrome ? 'karma-chrome-launcher' : 'karma-firefox-launcher'
+        ],
+        files: [
+            'node_modules/babel-polyfill/dist/polyfill.min.js',
+            'node_modules/qunitjs/qunit/qunit.js',
+            'node_modules/dexie/dist/dexie.js',
+            'test/karma/karma-env.js',
+            { pattern: 'test/worker.js', watched: true, included: false, served: true },
+            { pattern: '!(node_modules|tmp)*/*.map', watched: false, included: false, served: true },
+            'dist/dexieORMWrapper.js',
+            'test/tmp/tests.js',
+            { watched: true, included: false, served: true, pattern: 'test/worker.js' }
+        ],
+        basePath: '../..',
+        browsers: [ testWithChrome ? 'Chrome' : 'Firefox' ]
+    })
 }
