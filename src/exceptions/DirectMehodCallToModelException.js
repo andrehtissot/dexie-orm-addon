a553e401d157fa22101989c3b1a6274ad3384e9d
André Augusto Tissot <andrehtissot@gmail.com>
2018-01-07 19:22:23 +0100

    Now using template literals

--- src/exceptions/DirectMehodCallToModelException.js
+++ src/exceptions/DirectMehodCallToModelException.js
@@ -11,7 +11,7 @@
 
 export default class DORMWDirectMehodCallToModelException extends Error {
     constructor(methodName, ...params) {
-        super(methodName+" should only be called from a class that extends Model", ...params);
+        super(`${methodName} should only be called from a class that extends Model`, ...params);
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, this.constructor);
         }
--- src/exceptions/MethodMustBeImplementedException.js
+++ src/exceptions/MethodMustBeImplementedException.js
@@ -11,7 +11,7 @@
 
 export default class DORMWMethodMustBeImplementedException extends Error {
     constructor(functionName, ...params) {
-        super(functionName + " must be implemented in the extending class", ...params);
+        super(`${functionName} must be implemented in the extending class`, ...params);
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, this.constructor);
         }
