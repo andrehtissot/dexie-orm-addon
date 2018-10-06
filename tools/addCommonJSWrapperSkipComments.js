eb05c7c0131591718d4a4f04492f100f97b60c95
André Augusto Tissot <andre.tissot@mytheresa.com>
2018-10-06 20:44:53 +0200

    - 'tools/insertBanner.js' now works on windows; - babel-related wrapping methods now are ignored on test coverage

---
+++ tools/addCommonJSWrapperSkipComments.js
@@ -0,0 +1,18 @@
+const fs = require('fs'),
+    files = process.argv.slice(2);
+
+const ignoreComment = "/* istanbul ignore next */\n"
+
+const referenceTexts = [
+    'if (typeof define === "function" && define.amd) {',
+    'function _asyncToGenerator(fn)',
+    'var _slicedToArray = function () {'
+]
+
+files.forEach(file => {
+    let fileContent = fs.readFileSync(file, "utf-8");
+    for(let referenceText of referenceTexts) {
+        fileContent = fileContent.replace(referenceText, ignoreComment + referenceText);
+    }
+    fs.writeFileSync(file, fileContent, "utf-8");
+});
--- tools/insertBanner.js
+++ tools/insertBanner.js
@@ -1,12 +1,16 @@
 const fs = require('fs'),
     files = process.argv.slice(2),
-    bannerContent = fs.readFileSync('tools/banner.txt', "utf-8"),
-    escapeStringRegexp = require('escape-string-regexp'),
-    escapedBannerContent = escapeStringRegexp(bannerContent),
-    bannerRegexp = new RegExp(escapedBannerContent.replace(/ \\\*/g, " +\\*"), 'g');
+    bannerContent = fs.readFileSync('tools/banner.txt', 'utf-8')
+
+const removeBannerDuplicates = function(fileContent) {
+	if(fileContent.indexOf('/*!') === -1) {
+		return fileContent
+	}
+	return removeBannerDuplicates(fileContent.substring(0, fileContent.indexOf('/*!'))
+		+ fileContent.substring(fileContent.indexOf(' */') + 3));
+}
 
 files.forEach(file => {
-    let fileContent = fs.readFileSync(file, "utf-8");
-    fileContent = fileContent.replace(bannerRegexp, '');
-    fs.writeFileSync(file, bannerContent + fileContent, "utf-8");
+    let fileContent = fs.readFileSync(file, 'utf-8');
+    fs.writeFileSync(file, bannerContent + removeBannerDuplicates(fileContent), 'utf-8');
 });
